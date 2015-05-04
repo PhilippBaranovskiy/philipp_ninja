a72e306edc362fb17f321407cc58425d84449b26
Phil Baranovskiy <phil@lunarians.ru>
2015-05-05 00:54:25 +0300

    likely has been added

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+<?php if (!is_home()) : ?>
 <footer class="pageFooter">
 	<!-- <p>
 		<span class="iconsSET">H</span><br>
@@ -11,6 +12,7 @@
 		<!-- <div class="pinterest" data-media="i/pinnable.png">Запинить</div> -->
 	</div>
 </footer>
+<?php endif; ?>
 
 	<!-- Yandex.Metrika counter -->
 	<script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter28181988 = new Ya.Metrika({id:28181988, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.body, s = d.createElement("script"), f = function () { n.appendChild(s); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/28181988" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
