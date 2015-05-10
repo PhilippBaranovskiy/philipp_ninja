f9881f4f95e510300dd7b96db5891a77d86974fb
Phil Baranovskiy <phil@lunarians.ru>
2015-05-10 20:02:56 +0300

    home page look changed

--- home.php
+++ home.php
@@ -1,34 +1,29 @@
 <?php get_header(); ?>
 	<div class="info">
-		<p>Philipp Baranovskiy</p>
-		<p style="font-size: 0.6em; margin-top: -15px;">Software engineer / UI developer.</p>
+		<h1 class="info__name">Philipp Baranovskiy</h1>
+		<p class="info__status">Software engineer / UI developer</p>
 		<ul class="info__list">
 			<li class="info__list__item"><li class="info__list__item">
 				<a title="GitHub" href="https://github.com/rockfield" class="info__list__item__link info__list__item__link--icon sprite-gh"></a>
-				/ <a class="info__list__item__vision-link" href="https://github.com/rockfield">rockfield</a>
 			</li>
 			<li class="info__list__item">
 				<a title="LinkedIn" href="https://linkedin.com/in/philippninja" class="info__list__item__link info__list__item__link--icon sprite-in"></a>
-				/ <a class="info__list__item__vision-link" href="https://linkedin.com/in/philippninja">philippninja</a>
 			</li>
 			<li class="info__list__item">
 				<a title="MoiKrug" href="http://phil-b.moikrug.ru/" class="info__list__item__link info__list__item__link--icon sprite-mk"></a>
-				&nbsp;&nbsp;&nbsp;<a class="info__list__item__vision-link" href="http://phil-b.moikrug.ru/">phil-b</a>
 			</li>
-			<li>&nbsp;</li>
 			<li class="info__list__item"><li class="info__list__item">
 				<a title="VK" href="https://vk.com/philipp.ninja" class="info__list__item__link info__list__item__link--icon sprite-vk"></a>
-				/ <a class="info__list__item__vision-link" href="https://vk.com/philipp.ninja">philipp.ninja</a>
 			</li>
-			<li>&nbsp;</li>
-			<li class="info__list__item">
-				<a title="Email me" href="mailto:me@philipp.ninja" class="info__list__item__link info__list__item__link--icon sprite-email"></a>
-				<a class="info__list__item__vision-link" href="mailto:me@philipp.ninja">me@philipp.ninja</a>
+			<li class="info__list__item"><li class="info__list__item">
+				<a title="Istagram" href="https://instagram.com/philipp.ninja" class="info__list__item__link info__list__item__link--icon ig-b-24"></a>
 			</li>
+		</ul>
+		<ul class="info__list">
  			<li class="info__list__item">
-				<a title="Viber: open via mobile app" href="tel:375257689528" class="info__list__item__link info__list__item__link--icon sprite-viber"></a>
-				<a title="WhatsApp: open via mobile app" href="tel:375257689528" class="info__list__item__link info__list__item__link--icon sprite-wapp"></a>
 				<a class="info__list__item__vision-link" href="tel:375257689528">+375 (25) 768-95-28</a>
+				/
+				<a class="info__list__item__vision-link" href="mailto:me@philipp.ninja">me@philipp.ninja</a>
 			</li>
 		</ul>
 	</div>
