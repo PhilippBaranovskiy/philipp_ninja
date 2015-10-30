88ae85a45213a2f31decbc505a51b91696a8ad6d
rockfield <me@philipp.ninja>
2015-10-31 02:34:21 +0300

    no analytic of site owner usage

--- footer.php
+++ footer.php
@@ -15,9 +15,10 @@
 </footer>
 <?php endif; ?>
 	<?php wp_footer(); ?>
-
-	<!-- Yandex.Metrika counter -->
-	<script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter28181988 = new Ya.Metrika({id:28181988, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.body, s = d.createElement("script"), f = function () { n.appendChild(s); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/28181988" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-	<!-- /Yandex.Metrika counter -->
+	<?php if ( !is_admin() ) : ?>
+		<!-- Yandex.Metrika counter -->
+		<script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter28181988 = new Ya.Metrika({id:28181988, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.body, s = d.createElement("script"), f = function () { n.appendChild(s); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/28181988" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+		<!-- /Yandex.Metrika counter -->
+	<?php endif; ?>
 </body>
 </html>
